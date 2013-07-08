5061ee7e434ca3ec0aabe7be21ad05ddfdb71b14
Michael Doube <michael@doube.net>
2013-07-08 13:19:37 +0100

    HistoMapper: stub class

---
+++ src/org/bonej/histomorphometry/MapBoneMicrostructure.java
@@ -0,0 +1,13 @@
+package org.bonej.histomorphometry;
+
+public class MapBoneMicrostructure {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
